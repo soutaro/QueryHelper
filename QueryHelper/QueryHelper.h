a7e0a0da2497b04a29e9c99a186613a696a1b4ac
Soutaro Matsumoto <matsumoto@soutaro.com>
2012-05-06 13:02:43 +0900

    Initial Commit

---
+++ QueryHelper/QueryHelper.h
@@ -0,0 +1,13 @@
+//
+//  QueryHelper.h
+//  QueryHelper
+//
+//  Created by 宗太郎 松本 on 12/05/06.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QueryHelper : NSObject
+
+@end
---
+++ QueryHelper/QueryHelper.m
@@ -0,0 +1,13 @@
+//
+//  QueryHelper.m
+//  QueryHelper
+//
+//  Created by 宗太郎 松本 on 12/05/06.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "QueryHelper.h"
+
+@implementation QueryHelper
+
+@end
